f62e623ffbcbf40841f29574f268a275d9938279
yerkobits <yerkobits@gmail.com>
2021-09-27 20:41:51 -0400

    Block Explorer 2020

---
+++ config.js
@@ -0,0 +1,6 @@
+// ProsusCorp
+var api = 'http://186.64.120.70:16181';
+var blockTargetInterval = 120;
+var coinUnits = 1000000000000; // 12 decimals units
+var symbol = 'PROSUS';
+var refreshDelay = 30000;
